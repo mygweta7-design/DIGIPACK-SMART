456039b3572c7cef3347dc4f7b3786672bba6b21
mygweta7-design <mygweta7@gmail.com>
2026-05-14 13:50:30 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open('digipack-v4').then(cache => cache.addAll(['/', '/index.html', '/manifest.json'])));
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
