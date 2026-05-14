222d99324af08fd90b32f165b4af3b936482e4ad
mygweta7-design <mygweta7@gmail.com>
2026-05-14 13:24:59 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open('digipack-v3').then(cache => cache.addAll(['/', '/index.html', '/manifest.json'])));
+  event.waitUntil(caches.open('digipack-v4').then(cache => cache.addAll(['/', '/index.html', '/manifest.json'])));
 });
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
